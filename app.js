50a10263a64e891c90175c61fceaaf18bf068a8b
HoffenheimTechnologyLtd <ayeola2969529697@gmail.com>
2020-08-20 12:36:22 +0100

    added an id to the h1 tag and added the p tag, also added some dynamic functionality with javascript

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("HELLO")
+document.querySelector("#btn").addEventListener("click", function(){
+    document.querySelector("h1").textContent = "Kenny Changed this text to green";
+    document.querySelector("h1").style["color"] = "green";
+    setTimeout(()=>{
+        document.querySelector("p").textContent = "Please Wait.....";
+    }, 1000);
+    setTimeout(()=>{
+        document.querySelector("p").textContent = "Loading.....";
+    }, 2000);
+    setTimeout(()=>{
+        document.querySelector("p").textContent = "Done.";
+    }, 4000);
+    setTimeout(()=>{
+        document.querySelector("h2").textContent = "Kenny Changed this text to yellow";
+        document.querySelector("h2").style["color"] = "yellow";
+        document.querySelector("p").textContent = "";
+    }, 5000);
+})
